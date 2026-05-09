1bd790d0e081131b665f22919751c78d97b79ce5
Abdoae35 <Abdelrahman3535.elsheref@gmail.com>
2026-05-09 20:31:19 +0300

    cart

--- lib/features/cart/data/repo/cart_repo.dart
+++ lib/features/cart/data/repo/cart_repo.dart
@@ -1,15 +1,79 @@
 import 'package:bookia/core/services/apis/apis.dart';
 import 'package:bookia/core/services/apis/dio_provider.dart';
+import 'package:bookia/core/services/local/shared_pref.dart';
+import 'package:bookia/features/cart/data/models/cart_response/cart_response.dart';
 
 class CartRepo {
   // make three function to add to cart, remove from cart and get cart items
 
-  static getCart() async {
-    var response = await DioProvider.get(endpoint: Apis.showCart);
+  static Future<CartResponse?> getCart() async {
+    try {
+      var response = await DioProvider.get(
+        endpoint: Apis.showCart,
+        headers: {"Authorization": "Bearer ${SharedPref.getToken()}"},
+      );
+
+      if (response.statusCode == 200) {
+        return CartResponse.fromJson(response.data);
+      } else {
+        return null;
+      }
+    } on Exception catch (e) {
+      return null;
+    }
   }
 
-  static addToCart() {}
-  static removeFromCart() {}
+  static Future<CartResponse?> addToCart(int productId) async {
+    try {
+      var response = await DioProvider.post(
+        endpoint: Apis.addToCart,
+        headers: {"Authorization": "Bearer ${SharedPref.getToken()}"},
+        data: {"product_id": productId},
+      );
 
-  static updateCart() {}
+      if (response.statusCode == 201) {
+        return CartResponse.fromJson(response.data);
+      } else {
+        return null;
+      }
+    } on Exception catch (e) {
+      return null;
+    }
+  }
+
+  static Future<CartResponse?> removeFromCart(int cartItemId) async {
+    try {
+      var response = await DioProvider.delete(
+        endpoint: Apis.removeFromCart,
+        headers: {"Authorization": "Bearer ${SharedPref.getToken()}"},
+        data: {"cart_item_id": cartItemId},
+      );
+
+      if (response.statusCode == 200) {
+        return CartResponse.fromJson(response.data);
+      } else {
+        return null;
+      }
+    } on Exception catch (e) {
+      return null;
+    }
+  }
+
+  static Future<CartResponse?> updateCart(int cartItemId, int quantity) async {
+    try {
+      var response = await DioProvider.put(
+        endpoint: Apis.updateCart,
+        headers: {"Authorization": "Bearer ${SharedPref.getToken()}"},
+        data: {"cart_item_id": cartItemId, "quantity": quantity},
+      );
+
+      if (response.statusCode == 200) {
+        return CartResponse.fromJson(response.data);
+      } else {
+        return null;
+      }
+    } on Exception catch (e) {
+      return null;
+    }
+  }
 }
